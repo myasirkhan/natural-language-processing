7fe74a677854093172ffbc260524a98b046fd429
Yasir <memyselfandyasir@hotmail.com>
2017-12-07 22:22:16 +0500

    spark load logs

--- spark.py
+++ spark.py
@@ -1,5 +1,6 @@
 
-import sys, datetime
+import sys
+from datetime import datetime
 from operator import add
 
 from pyspark.sql import SparkSession
@@ -14,9 +15,45 @@ def f2(_):
 
 
 def parse_log(log):
-    print (log[0], log[3])
-    # return (log[0], datetime.strptime(log[3].replace("[", ""), '%d/%b/%Y:%H:%M:%S'))
-    return (log[0], log[3])
+    try:
+        # [u'323.81.303.680', '-', '-', 3'[25/Oct/2011:01:41:00', 4'-0500]', 5'"GET', 6'/download/download6.zip', 'HTTP/1.1"', 8'200', u'0', u'"-"', 11'"Mozilla/5.0', u'(Windows;', u'U;', 14'Windows', 15'NT', 16'5.1;', u'en-US;', u'rv:1.9.0.19)', u'Gecko/2010031422', 18'Firefox/3.0.19"']
+        # print ('ip_address: {}, date: {}, method: {}, path: {}, response_code: {}, browser: {}, operating_system: {}, browser_version: {}'.format(log[0], datetime.strptime(log[3][1:], '%d/%b/%Y:%H:%M:%S'), log[5][1:], log[6], log[8], log[11], '{} {}, {}'.format(log[14], log[15], log[16]), log[20]))
+        try:
+            ip = log[0]
+        except:
+            ip = None
+        try:
+            time_str = datetime.strptime(log[3][1:], '%d/%b/%Y:%H:%M:%S')
+        except:
+            time_str = None
+        try:
+            method = log[5][1:]
+        except:
+            method = None
+        try:
+            path = log[6]
+        except:
+            path = None
+        try:
+            response_code = log[8]
+        except:
+            response_code = None
+        try:
+            browser = log[11][1:]
+        except:
+            browser = None
+        try:
+            operating_system = '{} {}, {}'.format(log[14], log[15], log[16])
+        except:
+            operating_system = None
+        try:
+            browser_version = log[20]
+        except:
+            browser_version = None
+        return (ip, time_str, method, path, response_code, browser, operating_system, browser_version)
+    except:
+        print(log)
+        return (log[0], datetime.strptime(log[3][1:], '%d/%b/%Y:%H:%M:%S'), None, None, None, None, None, None)
 
 if __name__ == "__main__":
 
@@ -36,7 +73,7 @@ if __name__ == "__main__":
         # lines = spark.read.text("/Users/Yasir/Downloads/weblogs.txt").rdd.map(parse_log)
         # enTuples = spark.sparkContext.textFile("/Users/Yasir/Downloads/weblogs.txt").map(lambda line: line.split('\n'))
         # weblogdata = enTuples.map(parse_log)
-        df = spark.createDataFrame(raw_rdd, ["ip", "date"])
+        df = spark.createDataFrame(raw_rdd, ['ip_address', 'date', 'method', 'path', 'response_code', 'browser', 'operating_system', 'browser_version'])
 
         df.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
     else:
